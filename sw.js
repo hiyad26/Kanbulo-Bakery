dcb2197056b54a4372a78a76a58ef35976457795
hiyad26 <hiyad26@gmail.com>
2026-09-06 03:53:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,45 @@
-const CACHE_NAME = "kanbulo-bakery-v1";
+
+const CACHE_NAME = "kanbulo-bakery-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+    caches.match(event.request).then(cachedResponse => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        })
+      );
+    })
   );
 });
